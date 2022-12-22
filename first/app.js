d3c90a9860aa52af1792246c0d6d59b3626a7b7e
chaitanyawalke <chaitanyawalke61@gmail.com>
2022-12-22 15:54:02 +0530

    express file added

--- first/app.js
+++ first/app.js
@@ -1,3 +1,11 @@
 var express = require("express");
 var app = express();
 
+app.get("/",function(req,resp){
+
+    resp.send("<h1> hi chaitanya </h1>");
+    resp.end();
+})
+app.listen(3000);
+console.log("port are running in 3000 port");
+
